b3c2119852a44a542c18c909d57d2ba61add3354
JIHYEON-PF <108737977+JIHYEON-PF@users.noreply.github.com>
2023-01-29 17:55:24 +0900

    Solved/boj 2493 (#13)
    * solved: boj-10828 is solved
    
    * solved:boj_2493
    
    * refactor: move package boj_2493
    
    ---------
    
    Co-authored-by: JIHYEON <jhkim@aptner.com>

---
+++ solved/boj_2493.java
@@ -0,0 +1,40 @@
+package solved;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class boj_2493 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Stack<int[]> stack = new Stack<>();
+
+        for (int i = 1 ; i <= N ; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty()) {
+                if (stack.peek()[1] >= value) {
+                    bw.write(stack.peek()[0] + " ");
+                    break;
+                }
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                bw.write("0 ");
+            }
+            stack.push(new int[] {i, value});
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
