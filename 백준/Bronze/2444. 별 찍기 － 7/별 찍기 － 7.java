9ba92f97c86336282c75347fd4e8c99b392e82bc
JIHYEON-PF <108737977+JIHYEON-PF@users.noreply.github.com>
2023-02-21 00:40:37 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 84 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = N - i - 1 ; j > 0 ; j--) {
+                bw.write(" ");
+            }
+            for (int j = 0 ; j <= 2 * i ; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        for (int i = N ; i > 0 ; i--) {
+            for (int j = 0 ; j < N - i + 1 ; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0 ; j < i * 2 - 3 ; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
