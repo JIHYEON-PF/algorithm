ac97e471f53269615cc078f8a08e078ad0e9bba9
hyeon <108737977+JIHYEON-PF@users.noreply.github.com>
2023-03-06 15:22:55 +0900

    [level 0] Title: 7의 개수, Time: 2.92 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] array) {
+        
+        return (int) Arrays.stream(Arrays.stream(array).mapToObj(String::valueOf)
+                                   .collect(Collectors.joining())
+                                   .split(""))
+            .filter(s -> s.equals("7"))
+            .count();
+    }
+}
